7bcde6c77fb188ab60f35ca4cca8b031c4ca7616
William Beltran <w.beltran17@gmail.com>
2020-10-22 18:32:26 -0500

    first commit

---
+++ src/main/java/com/makingideas/ourark/OurArkApplication.java
@@ -0,0 +1,13 @@
+package com.makingideas.ourark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurArkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurArkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makingideas/ourark/OurArkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makingideas.ourark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurArkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
